7d4a4091bc84f07ea621f3282bc17219f0f367cf
wesley <wesley.lima@gmail.com>
2024-05-01 22:36:06 -0300

    Desafio confecção de uma conta

--- src/ContaBanco/ContaTerminal.java
+++ src/ContaBanco/ContaTerminal.java
@@ -1,7 +1,39 @@
 package ContaBanco;
+import java.util.Scanner;
 
- public class ContaTerminal {
-	 public static void main(String[] args) throws Exception {
-		System.out.println("HELLO WORLD");
+public class ContaTerminal {
+     
+     public static void main(String[] args) throws Exception {
+         
+         double saldoCliente = 237.4;
+         
+         Scanner scanner = new Scanner(System.in);
+         
+         System.out.println("Por favor, insira seu nome: ");
+         String nomeCliente = scanner.nextLine();
+         
+         System.out.println("Por favor, insira sua conta: ");
+         int contaCliente = scanner.nextInt();
+         
+         scanner.nextLine(); 
+         
+         System.out.println("Por favor, insira sua agencia: ");
+         String agenciaCliente = scanner.nextLine();
+         
+         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agenciaCliente
+                + " e conta " + contaCliente + ". Seu saldo de " + saldoCliente + " já está disponível para saque.");
+         scanner.close();
+     }
+
+
+	       
+	       
+	   
+	       
+	       
+	       
+	        
+	      
+
 	}
- }
+ 
